77bd7f501d55b57dc275587ae0364c4cad6358c9
Byron Karlen <byronkarlen@gmail.com>
2025-09-09 17:57:34 -0700

    map F to format thru lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -92,6 +92,7 @@ return {
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'F', vim.lsp.buf.format, opts)
           vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
           vim.keymap.set('n', '<leader>k', vim.diagnostic.open_float, opts)
         end

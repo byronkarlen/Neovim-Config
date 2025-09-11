ea992d0a759bd81ebb404084fe0836600701a894
Byron Karlen <byronkarlen@gmail.com>
2025-09-10 21:28:31 -0700

    change hover buffer to have a border

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -91,7 +91,11 @@ return {
           vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'K', function()
+            vim.lsp.buf.hover({
+              border = "rounded"
+            })
+          end, opts)
           vim.keymap.set('n', 'F', vim.lsp.buf.format, opts)
           vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
           vim.keymap.set('n', '<leader>k', function()

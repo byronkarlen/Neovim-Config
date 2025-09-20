de192a0034a191afe9a190d6b6c85d39569a632a
Byron Karlen <byronkarlen@gmail.com>
2025-09-20 14:27:39 -0700

    norddark

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -5,7 +5,7 @@ return {
   priority = 1000,
   config = function()
     if vim.g.current_theme == "nord" then
-      vim.g.nord_contrast = true
+      vim.g.nord_contrast = false
       vim.g.nord_borders = false
       vim.g.nord_disable_background = false
       vim.g.nord_italic = false
--- lua/theme.lua
+++ lua/theme.lua
@@ -5,5 +5,11 @@ local function get_system_appearance()
   return result:match("Dark") and "dark" or "light"
 end
 
-vim.g.current_theme = get_system_appearance() == "dark" and "nord" or "catppuccin"
+-- vim.g.current_theme = get_system_appearance() == "dark" and "nord" or "catppuccin"
 
+vim.opt.background = "dark"
+vim.g.current_theme = "nord"
+
+-- vim.opt.background = "light"
+-- vim.g.current_theme = "catppuccin"
+-- vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr:hor20-Cursor"
